7fc5419cfb16d85083435ffa2b26e6f95af64e2c
Oeoes Roy <oeoesroy@gmail.com>
2020-12-01 11:04:18 +0700

    circle

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -34,7 +34,7 @@
             <ul class="nav nav-treeview">
               <li class="nav-item">
                 <a href="" class="nav-link">
-                  <i class="far fa-arrow nav-icon"></i>
+                  <i class="far fa-circle nav-icon"></i>
                   <p>Spraying</p>
                 </a>
               </li>
