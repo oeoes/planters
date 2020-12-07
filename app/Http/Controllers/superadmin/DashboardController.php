5e2d7b95428cdb4c536e4090ae2ca332096fb158
alfinchandra4 <alfinchandra4@gmail.com>
2020-12-07 11:03:54 +0700

    seeder for managers

--- app/Http/Controllers/superadmin/DashboardController.php
+++ app/Http/Controllers/superadmin/DashboardController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\superadmin;
 use App\Http\Controllers\Controller;
 use App\Models\BlockReference;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index() {
-        $blockref = BlockReference::all();
+        $blockref = BlockReference::select('planting_year', DB::raw("sum('total_coverage')"))
+                                 ->groupBy('planting_year')
+                                 ->get();
+        dd($blockref->toArray());
         return view('superadmin.dashboard.index');
     }
 
